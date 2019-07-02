9f081269e5d01eed8e20b9d55e11254b855faaa1
YovchoGandjurov <yovchoo@gmail.com>
2019-07-02 16:19:27 +0300

    added tasks

--- 02.Basic CRUD/exercises.sql
+++ 02.Basic CRUD/exercises.sql
@@ -55,23 +55,100 @@ SELECT first_name, last_name FROM employees
 WHERE manager_id IS NULL;
 
 
+#11. Find All Employees with salary More Than 50000
+SELECT first_name, last_name, salary
+FROM employees
+WHERE salary > 50000
+ORDER BY salary DESC;
 
 
+#12. Find 5 Best Paid Employees
+SELECT first_name, last_name FROM employees
+ORDER BY salary DESC
+LIMIT 5;
 
 
+#13. Find All Employees Except Marketing
+SELECT first_name, last_name FROM employees
+WHERE department_id != 4;
 
 
+#14.Sort Employees Table
+SELECT * FROM employees
+ORDER BY 
+	salary DESC,
+    first_name,
+    last_name DESC,
+    middle_name,
+    employee_id;
 
 
+#15. Create View Employees with Salaries
+CREATE VIEW v_employees_salaries AS
+SELECT first_name, last_name, salary
+FROM employees;
+
+SELECT * FROM v_employees_salaries;
 
 
+#16. Create View Employees with Job Titles
+CREATE VIEW v_employees_job_titles AS
+SELECT concat_ws(' ', first_name, IFNULL(middle_name, ''), last_name) AS full_name, job_title
+FROM employees;
+
+SELECT * FROM v_employees_job_titles;
 
 
+#17. Distinct Job Titles
+SELECT DISTINCT job_title FROM employees
+ORDER BY job_title;
 
 
+#18.Find First 10 Started Projects
+SELECT * FROM projects
+ORDER BY start_date, name
+LIMIT 10;
 
 
+#19. Last 7 Hired Employees
+SELECT first_name, last_name, hire_date FROM employees
+ORDER BY hire_date DESC
+LIMIT 7;
 
+
+#20. Increase Salaries
+UPDATE employees
+	SET salary = salary * 1.12
+WHERE department_id IN (1, 2, 4, 11);
+
+SELECT salary FROM employees;
+
+
+USE geography;
+
+#21 All Mountain Peaks
+SELECT peak_name FROM peaks
+ORDER BY peak_name;
+
+
+#22. Biggest Countries by Population
+SELECT country_name, population FROM countries
+WHERE continent_code = 'EU'
+ORDER BY population DESC, country_name
+LIMIT 30;
+
+
+#23. Countries and Currency (Euro / Not Euro)
+SELECT country_name, country_code, IF(currency_code = 'EUR', 'Euro', 'Not Euro') AS currency
+FROM countries
+ORDER BY country_name;
+
+
+USE diablo;
+#24. All Diablo Characters
+
+SELECT name FROM characters
+ORDER BY name;
 
 
 
