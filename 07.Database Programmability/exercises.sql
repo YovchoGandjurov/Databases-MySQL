6cbc8537a8142fb0668031552ed115337884fde4
YovchoGandjurov <yovchoo@gmail.com>
2019-07-11 01:37:07 +0300

    Added more tasks

--- 07.Database Programmability/exercises.sql
+++ 07.Database Programmability/exercises.sql
@@ -1,3 +1,5 @@
+USE soft_uni;
+
 # 1.	Employees with Salary Above 35000
 
 DELIMITER $$
@@ -49,31 +51,52 @@ END $$
 
 
 #5.	Salary Level Function
-# CREATE FUNCTION ufn_get_salary_level(emp_salary DOUBLE)
+
+DELIMITER $$
+CREATE FUNCTION ufn_get_salary_level(emp_salary DOUBLE)
+RETURNS VARCHAR(8)
+BEGIN
+	DECLARE result VARCHAR(8);
+    SET result := IF(emp_salary < 30000, 'Low', IF(emp_salary BETWEEN 30000 AND 50000, 'Average', 'High'));
+    RETURN result;
+END $$
+
+#SELECT ufn_get_salary_level(13500.00);
 
 
+# 6.Employees by Salary Level
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_by_salary_level(salary_level VARCHAR(8))
+BEGIN
+		SELECT first_name, last_name FROM employees
+		WHERE ufn_get_salary_level(salary) = salary_level
+        ORDER BY first_name DESC, last_name DESC;
+END $$
+
+CALL usp_get_employees_by_salary_level('High');
 
 
+#7.	Define Function
+DELIMITER $$
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters VARCHAR(50), word VARCHAR(50))
+RETURNS INT
+BEGIN
+    DECLARE indx INT DEFAULT 1;
+    DECLARE symbol VARCHAR(1) DEFAULT '';
+    
+    WHILE indx <= char_length(word) DO
+		SET symbol := SUBSTRING(word, indx, 1);
+		IF LOCATE(symbol, set_of_letters) = 0 THEN
+			RETURN 0;
+		END IF;
+        
+        SET indx := indx + 1;
+    END WHILE;
 
+    RETURN 1;
+END $$
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+SELECT ufn_is_word_comprised('itmishof', 'sofia');
 
 
 
