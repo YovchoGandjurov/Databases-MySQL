6fd96c58b71e9754b90e3f864fac157a83a8755f
YovchoGandjurov <yovchoo@gmail.com>
2019-07-12 00:13:33 +0300

    Added some tasks

--- 07.Database Programmability/exercises.sql
+++ 07.Database Programmability/exercises.sql
@@ -99,47 +99,127 @@ END $$
 SELECT ufn_is_word_comprised('itmishof', 'sofia');
 
 
+#8.	Find Full Name
+DELIMITER $$
+CREATE PROCEDURE usp_get_holders_full_name()
+BEGIN
+	SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM account_holders
+    ORDER BY full_name, id;
+END $$
+
+CALL usp_get_holders_full_name();
+
+
+#9.	People with Balance Higher Than
+DELIMITER $$
+CREATE PROCEDURE usp_get_holders_with_balance_higher_than(num DOUBLE)
+BEGIN
+	SELECT ah.first_name, ah.last_name
+	FROM account_holders ah
+	JOIN (
+		SELECT id, account_holder_id, SUM(balance) AS balance
+		FROM accounts
+		GROUP BY account_holder_id
+		) a
+	ON ah.id = a.account_holder_id
+	WHERE balance > num
+	ORDER BY a.id, ah.first_name, ah.last_name;
+END $$
+
+CALL usp_get_holders_with_balance_higher_than(7000);
+
+
+#10. Future Value Function
+DELIMITER $$
+CREATE FUNCTION ufn_calculate_future_value(sum DOUBLE, int_rate DOUBLE, years INT)
+RETURNS DOUBLE
+BEGIN
+	DECLARE result DOUBLE;
+    SET result := sum * POW(1 + int_rate, years);
+    RETURN result;
+END; $$
+
+SELECT ufn_calculate_future_value(1000, 0.1, 5);
+
+
+#11. Calculating Interest
+DELIMITER $$
+CREATE PROCEDURE usp_calculate_future_value_for_account(acc_id INT, interest_rate DOUBLE(10,4))
+BEGIN
+	SELECT
+		a.id AS account_id,
+		ah.first_name,
+        ah.last_name,
+        a.balance AS current_balance,
+        ROUND(ufn_calculate_future_value(a.balance, interest_rate, 5), 4) AS balance_in_5_years
+	FROM account_holders ah
+    JOIN accounts a
+    ON ah.id = a.account_holder_id
+    WHERE a.id = acc_id;
+END $$
+
+CALL usp_calculate_future_value_for_account(1, 0.1);
+
+
+#12. Deposit Money
+
+DELIMITER $$
+CREATE PROCEDURE usp_deposit_money(account_id INT, money_amount DOUBLE(12, 4))
+BEGIN
+	START TRANSACTION;
+	IF money_amount <= 0 THEN
+	ROLLBACK;
+    ELSE
+		UPDATE accounts
+        SET balance = balance + money_amount
+        WHERE id = account_id;
+	END IF;
+END $$
+
+CALL usp_deposit_money(1, 10);
+
+
+#13. Withdraw Money
+DELIMITER $$
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DOUBLE(16,4))
+BEGIN
+	START TRANSACTION;
+		# SELECT balance
+        IF((SELECT balance FROM accounts WHERE id = account_id) < money_amount
+			OR money_amount <= 0) THEN
+		ROLLBACK;
+        ELSE
+			UPDATE accounts
+            SET balance = balance - money_amount
+            WHERE id = account_id;
+		END IF;
+END $$
+
+CALL usp_withdraw_money(1, 10);
+SELECT balance FROM accounts WHERE id = 1;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 14. Money Transfer
+DELIMITER $$
+CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT, amount DOUBLE(16,4))
+BEGIN
+	START TRANSACTION;
+    IF (from_account_id NOT IN (SELECT id FROM accounts) AND to_account_id NOT IN (SELECT id FROM accounts)) OR
+		amount < 0 OR
+		(SELECT balance FROM accounts WHERE id = from_account_id) < amount OR
+        from_account_id = to_account_id THEN
+        ROLLBACK;
+	ELSE
+		UPDATE accounts
+        SET balance = balance - amount
+        WHERE id = from_account_id;
+        
+        UPDATE accounts
+        SET balance = balance + amount
+        WHERE id = to_account_id;
+	END IF;
+END $$
 
 
 
